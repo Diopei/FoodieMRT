9b71d705a97f2f17859dff5b6cf40c88a400d3fd
diopei <diopeitw@gmail.com>
2023-08-03 09:39:00 +0800

    [dev]安裝element，並正式引入，使用單選匡

--- foodieMRT/src/components/FoodItem.vue
+++ foodieMRT/src/components/FoodItem.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+  import { ref } from 'vue'
+
+  const radio = ref(1)
+
   const listData = [
     {name: 'Tom', age: 18},
     {name: 'Jim', age: 20},
@@ -15,6 +19,15 @@
 </script>
 
 <template>
+
+  <div class="MRTradio">
+    <el-radio-group v-model="radio" justify="center">
+      <el-radio :label="1">台北捷運</el-radio>
+      <el-radio disabled :label="2">台中捷運</el-radio>
+      <el-radio disabled :label="3">高雄捷運</el-radio>
+    </el-radio-group>
+  </div>
+
   <div class="foodSection">
     <div class="foodItem" v-for="item in listData">{{item.name}}</div>
   </div>
