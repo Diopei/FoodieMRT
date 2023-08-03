698bf705d02400250230043537e45a211774cc98
diopei <diopeitw@gmail.com>
2023-08-03 09:00:32 +0800

    [dev]完成食物選單的按鈕

---
+++ foodieMRT/src/components/FoodItem.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  const listData = [
+    {name: 'Tom', age: 18},
+    {name: 'Jim', age: 20},
+    {name: 'Lucy', age: 16},
+    {name: 'Lucy', age: 16},
+    {name: 'Lucy', age: 16},
+    {name: 'Lucy', age: 16},
+    {name: 'Lucy', age: 16},
+    {name: 'Lucy', age: 16},
+    {name: 'Lucy', age: 16},
+    {name: 'Lucy', age: 16},
+    {name: 'Lucy', age: 16},
+  ]
+</script>
+
+<template>
+  <div class="foodSection">
+    <div class="foodItem" v-for="item in listData">{{item.name}}</div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- foodieMRT/src/views/HomeView.vue
+++ foodieMRT/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import FoodItem from "@/components/FoodItem.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+<!--    <TheWelcome />-->
+  <FoodItem />
   </main>
 </template>
